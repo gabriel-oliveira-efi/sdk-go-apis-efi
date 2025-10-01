f09836c471279891aac08562565e9441b048c0da
gabriel-oliveira-efi <gabriel.oliveira@sejaefi.com.br>
2025-10-01 15:44:56 -0300

    ✨ feat: creating "cob", "cobv", "config" examples

---
+++ examples/pix/split/cobv/pixSplitLinkDueCharge.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/efipay/sdk-go-apis-efi/examples/configs"
+	"github.com/efipay/sdk-go-apis-efi/src/efipay/pix"
+)
+
+func main() {
+
+	credentials := configs.Credentials
+	efi := pix.NewEfiPay(credentials)
+
+	const txid = ""
+	const splitConfigId = ""
+
+	res, err := efi.PixSplitLinkDueCharge(txid, splitConfigId)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
+}
---
+++ examples/pix/split/config/pixSplitConfigId.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/efipay/sdk-go-apis-efi/examples/configs"
+	"github.com/efipay/sdk-go-apis-efi/src/efipay/pix"
+)
+
+func main() {
+
+	credentials := configs.Credentials
+	efi := pix.NewEfiPay(credentials)
+
+	const id = ""
+
+	body := map[string]interface{}{
+		"descricao": "Waask",
+		"lancamento": map[string]interface{}{
+			"imediato": true,
+		},
+		"split": map[string]interface{}{
+			"divisaoTarifa": "assumir_total",
+			"minhaParte": map[string]interface{}{
+				"tipo":  "porcentagem",
+				"valor": "85.00",
+			},
+			"repasses": []map[string]interface{}{
+				{
+					"tipo":  "porcentagem",
+					"valor": "15.00",
+					"favorecido": map[string]interface{}{
+						"cpf":   "",
+						"conta": "",
+					},
+				},
+				{
+					"tipo":  "porcentagem",
+					"valor": "15.00",
+					"favorecido": map[string]interface{}{
+						"cpf":   "",
+						"conta": "",
+					},
+				},
+			},
+		},
+	}
+
+	res, err := efi.PixSplitConfigId(id, body)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res)
+	}
+}
